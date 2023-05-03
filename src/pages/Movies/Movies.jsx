7f1572d42c77d53b58e97b0ce13bf36440ed7822
Oleg <sirov@ukr.net>
2023-05-04 00:33:34 +0300

    update movies

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 
+// import { useSearchParams } from 'react-router-dom';
+// import { ProductList } from '../components/ProductList';
+// import { SearchBox } from '../components/SearchBox';
+
 // import { Backdrop, ModalContent } from './Modal.styled';
 
 const Movies = () => {
+  // const movies = getMovies();
+
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const movieName = searchParams.get('name') ?? '';
+
+  // const visibleMovies = movies.filter(movie =>
+  //   movie.name.toLowerCase().includes(productName.toLowerCase())
+  // );
+
+  // const updateQueryString = name => {
+  //   const nextParams = name !== '' ? { name } : {};
+  //   setSearchParams(nextParams);
+  // };
   return <>Movies</>;
 };
 export default Movies;
+
+//   return (
+//     <main>
+//       <SearchBox value={productName} onChange={updateQueryString} />
+//       <ProductList products={visibleProducts} />
+//     </main>
+//   );
+// };
